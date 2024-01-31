6a1a656745d174faca8182e91cb3fda220fb67d3
santiago-diaz-ar <santiagodiaz0201@gmail.com>
2024-01-30 23:43:19 -0500

    terminacion de restricciones en ttipo de datos

--- api/src/models/Activity.js
+++ api/src/models/Activity.js
@@ -2,18 +2,25 @@ const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
   sequelize.define("country", {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
     dificultad: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
     duracion: {
       type: DataTypes.NUMBER,
     },
     temporada: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
   });
 };
