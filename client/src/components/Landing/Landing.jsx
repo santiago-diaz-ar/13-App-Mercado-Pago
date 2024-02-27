6a310c16d46020b12183b69832b444dc78d62baa
santiago-diaz-ar <santiagodiaz0201@gmail.com>
2024-02-27 08:53:33 -0500

    desarrollando landing

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import style from "./Landing.module.css";
 
 function Landing() {
-  return <div>Landing En Desarrollo</div>;
+  return (
+    <div className={style.container}>
+      <div>Elizalde construcciones s.a.s</div>,
+      <div>Ingrewsa si quieres cotizar un terreno</div>
+    </div>
+  );
 }
 
 export default Landing;
